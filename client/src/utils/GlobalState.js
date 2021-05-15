35ef255ea0c0429e48e95b552948b9255fc6382c
AdamR-Work <aramosatwork@hotmail.com>
2021-05-15 15:24:10 -0500

    new branch

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -5,13 +5,15 @@ const StoreContext = createContext();
 const { Provider } = StoreContext;
 
 const StoreProvider = ({ value = [], ...props }) => {
-    const [state, dispatch] = useProductReducer({
+    const [state, dispatch] = useProductReducer({  // ln 33 reducers 
+      //changes it from reducer basically to use product reducer for use and readablity 
       products: [],
       categories: [],
       currentCategory: '',
     });
+
     // use this to confirm it works!
-    console.log(state);
+    console.log(state);// !!!! this happens twice because its the intial then new. From the Reducers? line 12 -13
     return <Provider value={[state, dispatch]} {...props} />;
   };
   const useStoreContext = () => {
